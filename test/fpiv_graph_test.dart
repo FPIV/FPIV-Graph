bf7129a9fe016a6a35fc9ebfc94924799b8143a7
MR.X <fpivos@gmail.com>
2025-09-26 05:54:02 +0300

    up

---
+++ test/fpiv_graph_test.dart
@@ -0,0 +1,16 @@
+import 'package:fpiv_graph/fpiv_graph.dart';
+import 'package:test/test.dart';
+
+void main() {
+  group('A group of tests', () {
+    final awesome = Awesome();
+
+    setUp(() {
+      // Additional setup goes here.
+    });
+
+    test('First Test', () {
+      expect(awesome.isAwesome, isTrue);
+    });
+  });
+}
